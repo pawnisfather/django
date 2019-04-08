da6e7b011346f05e152f71c6c1d3f02028766fb3
pawnisfather <583366099@qq.com>
2019-04-08 15:59:03 +0800

    新的分支

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,6 @@
+#!F:\code\django\django\venv\Scripts\python.exe
+# EASY-INSTALL-DEV-SCRIPT: 'Django==3.0.dev20190408000220','django-admin.py'
+__requires__ = 'Django==3.0.dev20190408000220'
+__import__('pkg_resources').require('Django==3.0.dev20190408000220')
+__file__ = 'F:\\code\\django\\django\\django\\bin\\django-admin.py'
+exec(compile(open(__file__).read(), __file__, 'exec'))
